8cc909e44c7d2c9a96f07f19ca2a6a087df1afdd
danielailie12 <danielailie12@gmail.com>
2015-03-10 15:40:53 +0200

    Personalities updated

---
+++ Personalities/Personality.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Personalities
+{
+    class Personality
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Dob { get; set; }
+        public string Dod { get; set; }
+
+        public List<Personality> Personalities= new List<Personality>();
+
+        public Personality(string firstName,string lastName,string dob,string dod)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Dob = dob;
+            Dod = dod;
+        }
+
+        public Personality()
+        {
+        }
+        public void ReadFromFile(string file)
+        {
+            using (var reader = new StreamReader(file))
+            {
+                string line;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    string[] parts = line.Split(',');
+                    var personality =new Personality(parts[0], parts[1] ,parts[2],parts[3]);
+                    Personalities.Add(personality);
+                }
+            }
+        }
+
+        public override string ToString()
+        {
+            return String.Format("{0} {1} ({2} - {3})", FirstName, LastName, Dob, Dod);
+        }
+    }
+}
--- Personalities/Program.cs
+++ Personalities/Program.cs
@@ -1,9 +1,20 @@
-﻿namespace Personalities
+﻿using System;
+
+namespace Personalities
 {
     class Program
     {
-        static void Main(string[] args)
+        private static string _filePath;
+
+        static void Main()
         {
+            var personality=new Personality();
+            _filePath = "C:\\Users\\daniela.ilie\\Documents\\visual studio 2013\\Projects\\Personalities\\Personalities\\bin\\Debug\\Personalities.csv";
+            personality.ReadFromFile(_filePath);
+            foreach (var p in personality.Personalities)
+            {
+                Console.WriteLine(p.ToString());
+            }
         }
     }
 }
